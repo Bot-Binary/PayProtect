0c795e41a7b436acb22137322384c1759fe0d50f
Jay Sanghani <90022040+Jaysanghani08@users.noreply.github.com>
2023-07-28 18:56:03 +0000

    added some form components

--- payprotectfrontend/src/App.js
+++ payprotectfrontend/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
 import Signup from './Components/Parent/SignUp/Signup';
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Signup />} />
-        </Routes>
+        <div>
+            <Signup />
+        </div>
     );
 }
 
