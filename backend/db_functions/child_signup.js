6ee79cac2c4382374e3ff01638c8999bf9d7cdf5
Shubham Patel <120598942+Bot-Binary@users.noreply.github.com>
2023-08-01 14:13:13 +0000

    child_signup done with schema

--- backend/db_functions/child_signup.js
+++ backend/db_functions/child_signup.js
@@ -5,10 +5,9 @@ const cors = require("cors");
 const { v4: uuidv4 } = require('uuid');
 
 
-
 // models
-const parent = require("../models/registration/parents");
 const child = require("../models/registration/child");
+const parent = require("../models/registration/parents");
 
 
 
@@ -17,26 +16,29 @@ const otp = require("../otp-mailer/otp");
 
 
 
-// app.post("/parent/signup",
-const parent_signup = async (req, res) => {
+
+const child_signup = async (req, res) => {
     const data = req.body;
     // const phone = req.body.phone;
     // const email = req.body.email;
 
-    const already_exist_phone = await child.findOne({ phone: req.body.phone });
-    const already_exist_email = await child.findOne({ email: req.body.email });
+    const already_exist_phone = await parent.findOne({ phone: req.body.phone });
+    const already_exist_email = await parent.findOne({ email: req.body.email });
+
+    console.log(already_exist_email);
+    console.log(already_exist_phone);
 
 
-    if (already_exist_email || already_exist_phone) {
-        res.status(399).send("Already Exist");
+
+    if (already_exist_phone != null || already_exist_email != null) {
+        res.status(399).send("Alredy Exist");
     }
-
     else {
 
         console.log(data);
 
-        const new_parent = new parent({
-            id: uuidv4() + "P",
+        const new_child = new child({
+            id: uuidv4() + "C",
             fname: data.fname,
             mname: data.mname,
             lname: data.lname,
@@ -50,13 +52,13 @@ const parent_signup = async (req, res) => {
         })
 
         try {
-            const save = await new_parent.save()
+            const save = await new_child.save()
                 .then(async () => {
-                    const x = await parent.find({ phone: data.phone });
+                    const x = await child.find({ phone: data.phone });
                     console.log("SAVED");
                     res.status(200).send(x);
                 }).catch((e) => {
-                    console.log("THIS IS ERROR FROM parent_signup.js file");
+                    console.log("THIS IS ERROR FROM child_signup.js file");
                     console.log(e);
                     const err = e.keyPattern;
                     // console.log(err);
@@ -69,18 +71,19 @@ const parent_signup = async (req, res) => {
                     else if (err.hasOwnProperty('username') == true && err.username == 1) {
                         res.status(412).send();
                     }
+                    else {
+                        res.status(413).send("Bad Request");
+                    }
 
-                    res.status(413).send("Bad Request");
 
                 })
 
         } catch (error) {
-            console.log("THIS IS ERROR FROM parent_signup.js");
+            console.log("THIS IS ERROR FROM child_signup.js");
             console.log(error);
         }
+
     }
-
-
 
     // console.log()
 
@@ -88,4 +91,4 @@ const parent_signup = async (req, res) => {
 // )
 
 
-module.exports = parent_signup;
+module.exports = child_signup;
