7a25740368b902114089c6f66f1c80f63196b1a5
Shubham Patel <120598942+Bot-Binary@users.noreply.github.com>
2023-09-18 16:41:24 +0000

    hh

--- backend/db_functions/otp_verification.js
+++ backend/db_functions/otp_verification.js
@@ -11,35 +11,42 @@ const merchant = require("../models/registration/merchant");
 
 
 const otp_verification = (async (req, res) => {
-    const phone_ = req.body.phone;
-    const phone = `+91${phone_}`;
-    const otp_number = req.body.otp;
 
-    const data = await otp.findOne({ phone: phone });
-    const hashed_otp = data.otp;
+    try {
+        const phone_ = req.body.phone;
+        // const phone = `+91${phone_}`;
+        const otp_number = req.body.otp;
 
-    const match = await bcrypt.compare(otp_number, hashed_otp);
-    if (match) {
-        res.status(200).send();
-    }
-    else {
+        const data = await otp.findOne({ phone: phone });
+        const hashed_otp = data.otp;
 
-        try {
-            const dlt = await parent.deleteOne({ phone: phone_ });
-            const dlt_count = dlt.deletedCount;
-            if (dlt_count == 0) {
-                const dlt = await child.deleteOne({ phone: phone_ });
-                if(dlt.deletedCount==0){
-                    const dlt = await merchant.deleteOne({phone : phone_});
-                }
-            }
-        } catch (error) {
-            console.log("This is error from otp_verification.js -> delete part");
-            console.log(error);
+        const match = await bcrypt.compare(otp_number, hashed_otp);
+        if (match) {
+            res.status(200).send();
         }
+        else {
+
+            try {
+                const dlt = await parent.deleteOne({ phone: phone_ });
+                const dlt_count = dlt.deletedCount;
+                if (dlt_count == 0) {
+                    const dlt = await child.deleteOne({ phone: phone_ });
+                    if (dlt.deletedCount == 0) {
+                        const dlt = await merchant.deleteOne({ phone: phone_ });
+                    }
+                }
+            } catch (error) {
+                console.log("This is error from otp_verification.js -> delete part");
+                console.log(error);
+            }
 
 
-        res.status(288).send();
+            res.status(288).send();
+        }
+    }
+    catch(e){
+        console.log("There is error in otp_verification");
+        console.log(e);
     }
 
 })
